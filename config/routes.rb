9f0eac4149add594c5f1daed03769578f1360d22
Kira Pilot <kira.pilot23@gmail.com>
2016-07-08 11:25:45 -0400

    added restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'restaurant/index'
+
+  # get 'restaurant/new'
+
+  # get 'restaurant/create'
+
+  # get 'restaurant/show'
+
+  resources :restaurants
+
   devise_for :owners
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
